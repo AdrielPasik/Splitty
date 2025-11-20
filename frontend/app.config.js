cd7ad342f1818064639e2eaaa46fd8e1ec2a3a8f
santinotanus <santinotanuslinares@hotmail.com>
2025-11-20 13:00:31 -0300

    BUILD APK

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -13,7 +13,7 @@ export default ({ config }) => ({
     // inesperados queden expuestos a la app (causa frecuente de errores).
     BACKEND_URL: process.env.EXPO_PUBLIC_BACKEND_URL ? String(process.env.EXPO_PUBLIC_BACKEND_URL) : (process.env.BACKEND_URL ? String(process.env.BACKEND_URL) : null),
     eas: {
-      projectId: "f40eab8a-fedd-44de-b378-996b1639a9a7"
+      projectId: "d4288f8f-e584-4de7-87eb-facedcec213e"
     }
   }
 });
